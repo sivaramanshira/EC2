8c61aef6cd23b953b9d00011a2e5edc2511b8144
sivaramanshira <34826837+sivaramanshira@users.noreply.github.com>
2021-08-24 11:08:11 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 # Add the -f option to curl if server errors like HTTP 404 should fail too
 sleep 30
 if curl -I "http://18.234.76.89:5000/"; then
-  echo " alive and web site is up"
+  echo "Web Application Alive!!"
 else
   echo "offline or web server problem"
   exit 1
